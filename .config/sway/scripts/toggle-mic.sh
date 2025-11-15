c4a3f46d2987973f0386cf8c63e70e68db25e6a4
PastArizox <arizoxfr@gmail.com>
2025-11-15 23:09:44 +0100

    feat: use sudo in toggle-mic script

--- .config/sway/scripts/toggle-mic.sh
+++ .config/sway/scripts/toggle-mic.sh
@@ -3,8 +3,8 @@
 STATE=$(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -o '\[MUTED\]')
 if [ "$STATE" = "[MUTED]" ]; then
     wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 0
-    echo 0 | tee /sys/class/leds/platform::micmute/brightness
+    echo 0 | sudo tee /sys/class/leds/platform::micmute/brightness
 else
     wpctl set-mute @DEFAULT_AUDIO_SOURCE@ 1
-    echo 1 | tee /sys/class/leds/platform::micmute/brightness
+    echo 1 | sudo tee /sys/class/leds/platform::micmute/brightness
 fi
